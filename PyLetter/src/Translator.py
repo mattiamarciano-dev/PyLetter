61022e291e3231aa5487e0bb3b36405d4c60000e
Mattia Felice Marciano <156821144+mattiamarciano-dev@users.noreply.github.com>
2026-07-24 23:54:38 +0200

    Delete PyLetter directory

--- PyLetter/src/Translator.py
+++
@@ -1,9 +0,0 @@
-from deep_translator import GoogleTranslator
-
-class Translator:
-    @staticmethod
-    def translate(content: str, source: str, target: str) -> str:
-        return GoogleTranslator(
-            source=source,
-            target=target
-        ).translate(content)
